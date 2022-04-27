ba7fd5e4a22c8967efd854cc45231c7b222ca17a
Madhav Ranka <madhav.ranka@payu.in>
2022-04-27 17:59:47 +0530

    Loan.php - Improve validation with Criterion and create repaymentSchedule

--- Application/Loan.php
+++ Application/Loan.php
@@ -2,11 +2,20 @@
 namespace MoneyLendingChallenge;
 
 include "utility/Validator.php";
+include "utility/RateOfInterest.php";
+include "utility/Criterion.php";
+include "utility/Response.php";
+
+use Criterion;
+use RateOfInterest;
+use Response;
 use Validator;
 
 class Loan
 {
-    //const MANDATORY_PARAMS = ['name','dob','city','score','amount'];
+    /** Request Specifications for Validation
+     * @var array
+     */
     public static array $validationSpecs = [
         "amount" => ['type' => 'fn', "class" => "Validator", "name" => "validateLoanAmount" ,
             "min-value" => 50000, "max-value" => 500000],
@@ -15,19 +24,44 @@ class Loan
         "name" => ["type" => "string", "min-length" => 1, "regex" => "^[a-zA-Z\s]*$^" ,"name" => "name"],
         "city" => ["type" => "string", "min-length" => 1, "name" => "city"]
     ];
-    private array $postParams = [];
+
+    private array $postParams;
+
     public function __construct()
     {
         $this->postParams = $_REQUEST;
     }
 
     public function main(){
-        print_r($this->validateRequest());
+        $validationResult = $this->validateRequest();
+        $this->processValidationFailure($validationResult);
+        $criterionUtil = new Criterion($this->postParams);
+        $validationResult = $criterionUtil->validateAgeAndCity();
+        $this->processValidationFailure($validationResult);
+        $interestUtil = new RateOfInterest($this->postParams['amount'], $criterionUtil->getCityTier(),
+            $this->postParams['score'] );
+        $repaymentSchedule = $interestUtil->createRepaymentSchedule();
+        Response::echoResponse([
+            'status' => "Approve",
+            "Rate of Interest" => $interestUtil->getRateOfInterest()."%",
+            'schedule' => $repaymentSchedule
+        ]);
     }
 
+    /** Validates request params
+     * @return array
+     */
     private function validateRequest(): array
     {
         $validator = new Validator();
         return $validator->validateSpec(self::$validationSpecs,$this->postParams);
+    }
+
+    private function processValidationFailure(array $validationResult)
+    {
+        if($validationResult['error']!=""){
+            $validationResult['status'] = "Reject";
+            Response::echoResponse($validationResult);
+        }
     }
 }
